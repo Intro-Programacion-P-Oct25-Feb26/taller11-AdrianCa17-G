a744d495d90e33cae26a97c4b67624f26795879a
AdrianCa17-G <adrian.steven.guaman.carrion@gmail.com>
2026-01-08 12:42:44 -0500

    Corte del taller

--- ejercicios/Problemas/src/paquete04/Problema4.java
+++ ejercicios/Problemas/src/paquete04/Problema4.java
@@ -5,6 +5,8 @@
  */
 package paquete04;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -16,6 +18,55 @@ public class Problema4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+
+        int opcion;
+
+        System.out.println("Seleccione una de las siguientes funciones:\n1. Calcular"
+                + "el valor de la planilla de luz\n2. Calcular el valor del predio"
+                + "de un bien inmueble\n");
+
+        opcion = entrada.nextInt();
+
+        if ((opcion == 1) || (opcion == 3)) {
+
+            String nombre;
+            String cedula;
+
+            System.out.println("Ingrese su nombre: ");
+            nombre = entrada.nextLine();
+            System.out.println("Ingrese su cedula: ");
+            cedula = entrada.nextLine();
+
+            if (opcion == 1) {
+                valorPlanillaLuz();
+            } else {
+                if (opcion == 2) {
+                    predioBienInmueble();
+
+                } else {
+                    System.out.println("Opcion Invalida");
+                }
+            }
+        }
     }
-    
+
+    public static void valorPlanillaLuz(String nombre, String apellido) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        int valorKilowatio;
+        int numeroKilowatios;
+        int respuestaPlanillaLuz;
+
+        System.out.println("Usted ha seleccionado la opcion de obtener la planilla de luz\n");
+        System.out.println("Ingrese el valor de los kilowatios");
+        valorKilowatio = entrada.nextInt();
+        System.out.println("Ingrese el numero de kilowatios");
+        numeroKilowatios = entrada.nextInt();
+        
+        
+        respuestaPlanillaLuz = valorKilowatio * numeroKilowatios;
+
+        System.out.println("El area del cuadrado es de: " + respuestaPlanillaLuz);
 }
